ac6baf28908a97b59a6abb476f85aa7c6283a6fb
ANNIJOICE <annijoice96@gmail.com>
2023-08-10 13:44:20 +0530

    Operators and subject

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Optional, inject } from '@angular/core';
-import { Observable, mergeMap, forkJoin, map, switchMap, concatMap, from, Subject, BehaviorSubject, ReplaySubject, AsyncSubject, of, filter } from 'rxjs';
+import { Observable, mergeMap, forkJoin, map, switchMap, concatMap, from, Subject, BehaviorSubject, ReplaySubject, AsyncSubject, of, filter, fromEvent, scan, throttleTime, count, delay, timer, mapTo, exhaustMap, zip, combineLatest } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { AppService } from './app.service';
 import { ActivatedRoute } from '@angular/router';
@@ -27,9 +27,13 @@ export class AppComponent implements OnInit{
     @Optional() private service1: AppService ) {}
   
   ngOnInit(): void {
-    this.fromOfDiff()
-    console.log(this.userService.getUserList());
 
+    // fromEvent(document, 'click').subscribe(() => console.log("clicked"));
+
+    // console.log(this.userService.getUserList());
+  }
+
+  subject() {
     //Subject op - 3
     console.log(" Subject starts")
 
@@ -40,7 +44,9 @@ export class AppComponent implements OnInit{
     })
     this.subjectData.next(3);
     this.subjectData.complete()
+  }
 
+  behaviourSubject() {
     //Behaviour op - 
     // Sub1 0
     // Sub1 1
@@ -64,7 +70,9 @@ export class AppComponent implements OnInit{
  
     this.behaviorSubjet.next("3");
     this.behaviorSubjet.complete();
+  }
 
+  replaySubject() {
     // Replay Subject
     console.log("Replay Subject starts")
     this.replaySubject$.next("1");
@@ -96,8 +104,9 @@ export class AppComponent implements OnInit{
       err => console.error("sub3 " + err),
       () => console.log("Complete")
     );
+  }
 
-
+  asyncsubject() {
     // Async Subject
     // Output
     // Sub1 5
@@ -105,6 +114,8 @@ export class AppComponent implements OnInit{
     // Sub1 Complete
     // Sub3 5
     // Sub3 Complete
+    console.log("Async Subject starts")
+
     this.asyncSubject.next("1");
     this.asyncSubject.next("2");
  
@@ -133,6 +144,172 @@ export class AppComponent implements OnInit{
       err => console.error("sub3 " + err),
       () => console.log("Sub3 Complete")
     );
+  }
+
+  scanOperator() {
+      // using scan => like reducer
+      fromEvent(document, 'click').pipe(
+        scan((count) => count + 1, 0)
+      ).subscribe((res) => console.log("FROM EVENT",res))
+  }
+
+  throttleTime() {
+    // to allow one click per second
+    fromEvent(document, 'click').pipe(
+      throttleTime(1000),
+      scan((count) => count + 1, 0)
+    ).subscribe((res) => console.log("Throttle time ", res))
+  }
+
+  delay() {
+    // emit value after 1 sec
+    fromEvent(document, 'click').pipe(
+      delay(1000)
+    ).subscribe((res) => console.log("clicked"))
+  }
+
+  ofOperator() {
+    // of operator
+    of(1,2,3).pipe(
+      map((x) => x * x)
+    ).subscribe((value) => {
+      console.log("Value is", value)
+    })
+  }
+
+  // most recent value will get returned
+  switchMap() {
+    const sub = new Subject<string>();
+
+    sub.asObservable().pipe(
+      switchMap((value) => this.anyLongRunningOp(value))
+    ).subscribe((res) => console.log("received value from switch map", res))
+
+    sub.next("HEllo from switch");
+    sub.next("Second hello from switch")
+    sub.next("Third hello from switch")
+
+  }
+
+  // emits value one by one - it like a queue after one completes it allows next
+  concatMap() {
+    const sub = new Subject<string>();
+
+    sub.asObservable().pipe(
+      concatMap((value) => this.anyLongRunningOp(value))
+    ).subscribe((res) => console.log("received value from concat map", res))
+
+    sub.next("HEllo from concat");
+    sub.next("Second hello from concat")
+    sub.next("Third hello from concat")
+  }
+
+  // runs in parellel, the order of the return values not guaranteed
+  mergeMap() {
+    const sub = new Subject<string>();
+
+    sub.asObservable().pipe(
+      mergeMap((value) => this.anyLongRunningOp(value))
+    ).subscribe((res) => console.log("received value from merge map", res))
+
+    sub.next("HEllo from merge");
+    sub.next("Second hello from merge")
+    sub.next("Third hello from merge")
+  }
+
+  // cancels latest request if current one is pending
+  exhaustMap() {
+    const sub = new Subject<string>();
+
+    sub.asObservable().pipe(
+      exhaustMap((value) => this.anyLongRunningOp(value))
+    ).subscribe((res) => console.log("received value from exhaust map", res))
+
+    sub.next("HEllo from exhaust");
+    sub.next("Second hello from exhaust")
+    sub.next("Third hello from exhaust")
+  }
+
+  // zip operator - Take a group of observables, Wait until all observables have emitted a value then emit all as an array.
+  /* 
+  The output:
+  1 2
+  3 4
+  5 6
+  7 8
+  */
+  zipOperator() {
+    const obs1$ = new Subject<string>();
+    const obs2$ = new Subject<string>();
+
+    zip(obs1$, obs2$).subscribe(([obs1Data, obs2Data]) => console.log("Zip Operator", obs1Data, obs2Data));
+    // Emit values
+    obs1$.next('1');
+    obs2$.next('2');
+
+    obs1$.next('3');
+    obs2$.next('4');
+
+    obs1$.next('5');
+    obs2$.next('6');
+
+    obs1$.next('7');
+    obs2$.next('8');
+
+    obs1$.next('9'); // This won't be displayed
+
+  }
+
+  // combineLatest - combineLatest() will wait until all observables have emitted a value as an array. From there, it will emit an array each time one observable emits a value
+  combineLastesOperator() {
+    const obs1$ = new Subject<string>();
+    const obs2$ = new Subject<string>();
+
+    combineLatest(obs1$, obs2$).subscribe(([obs1Data, obs2Data]) => console.log("combineLatest Operator", obs1Data, obs2Data));
+    // Emit values
+    obs1$.next('1');
+    obs2$.next('2');
+
+    obs1$.next('3');
+    obs2$.next('4');
+
+    obs1$.next('5');
+    obs2$.next('6');
+
+    obs1$.next('7');
+    obs2$.next('8');
+
+    obs1$.next('9');
+  }
+
+
+// Fork Join - Take a group of Observables, wait for them to complete emission and emit the final value of each Observable as an array.
+  forkJoinOperator() {
+    const obs1$ = new Subject<string>();
+    const obs2$ = new Subject<string>();
+
+    forkJoin(obs1$, obs2$).subscribe(([obs1Data, obs2Data]) => console.log("ForkJoin Operator", obs1Data, obs2Data));
+    // Emit values
+    obs1$.next('1');
+    obs2$.next('2');
+
+    obs1$.next('3');
+    obs2$.next('4');
+
+    obs1$.next('5');
+    obs2$.next('6');
+
+    obs1$.next('7');
+    obs2$.next('8');
+
+    obs1$.next('9');
+    obs1$.complete();
+    obs2$.complete();
+    // obs2$.error('error');
+  }
+
+  anyLongRunningOp(value: string) {
+    return timer(2000).pipe(mapTo(value));
   }
 
   fromOfDiff():void {
